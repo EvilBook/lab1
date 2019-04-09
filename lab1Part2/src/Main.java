1c0eb81bb70cbca26d97f1076c269733d10d5c65
szomer <suzannezomer@gmail.com>
2019-04-09 15:31:34 +0200

    scmicjmdkmckdcndk

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
 
 
-        System.out.println("pandas are fat");
+        System.out.println("pandas are not fat");
         System.out.println("dont swear");
 
         System.out.println("ze");
