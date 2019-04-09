066bf417ca719441c6b784ba7c632a6236088f36
Cassian <Louliett@gmail.com>
2019-04-09 15:26:46 +0200

    not cute

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
 
 
-        System.out.println("pandas are cute");
+        System.out.println("pandas arent cute");
         System.out.println("dont swear");
 
 
