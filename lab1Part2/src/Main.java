56740632f474641c4c40afb0ba842031e6fbe622
Cassian <Louliett@gmail.com>
2019-04-09 13:41:09 +0200

    my first commit

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args){
 
 
-        System.out.println("shits");
+        System.out.println("dont");
+        System.out.println("use");
+        System.out.println("swears");
 
     }
 
