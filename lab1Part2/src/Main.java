1381c3f7a6a1f9f0ef42da48b72594fc8bd290f3
EvilBook <martin.zannato@yahoo.com>
2019-04-09 14:28:40 +0200

    Class to be merged into the master branch

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 
         System.out.println("shits");
+        NewClassForMain newClassForMain=new NewClassForMain();
 
     }
 
---
+++ lab1Part2/src/NewClassForMain.java
@@ -0,0 +1,11 @@
+public class NewClassForMain {
+
+
+    public NewClassForMain(){
+
+
+        System.out.println("new Class for main Created");
+
+    }
+
+}
