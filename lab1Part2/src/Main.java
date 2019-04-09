af228e8046defa656239411c083543c23e1803fd
EvilBook <martin.zannato@yahoo.com>
2019-04-09 13:54:47 +0200

    accidental delete 2

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -3,13 +3,7 @@ public class Main {
 
     public static void main(String[] args){
 
-
-        System.out.println("dont");
-        System.out.println("use");
-        System.out.println("swears");
-        System.out.println("please");
-        System.out.println("fuck you");
-        System.out.println("revert");
+        System.out.println("bad move");
 
     }
 
