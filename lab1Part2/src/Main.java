109f74b7fc3d0834b85e9e59c0ae508dbf360dc1
szomer <suzannezomer@gmail.com>
2019-04-09 15:20:36 +0200

    .

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args){
 
 
-        System.out.println("took over bitch");
+        System.out.println("pandas are cute");
         System.out.println("dont swear");
-
-
 
         System.out.println("ze");
         NewClassForMain newClassForMain=new NewClassForMain();
