10ff74bfee2b3e51a7e0fbddbaca7615e5558a38
szomer <suzannezomer@gmail.com>
2019-04-09 15:27:45 +0200

    scmicjmdkmckdcndk

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
 
 
-        System.out.println("pandas are cute");
+        System.out.println("pandas are gay");
         System.out.println("dont swear");
 
         System.out.println("ze");
