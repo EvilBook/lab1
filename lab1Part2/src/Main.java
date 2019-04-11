43c1033b06cac3979d1a2f14032df9b4226a29d5
szomer <suzannezomer@gmail.com>
2019-04-11 13:44:02 +0200

    change

--- lab1Part2/src/Main.java
+++ lab1Part2/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
     public static void main(String[] args){
-        System.out.println("I decided everything is useless");
+        System.out.println("I decided everything is useless 389237");
     }
 
 }
